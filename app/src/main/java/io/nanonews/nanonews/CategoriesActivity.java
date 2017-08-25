5cecb4d793927788ff3b9736189015d702ae87af
Louis <louis993546@gmail.com>
2017-08-26 01:56:24 +0200

    ADDing: category ui & icons

---
+++ app/src/main/java/io/nanonews/nanonews/CategoriesActivity.java
@@ -0,0 +1,16 @@
+package io.nanonews.nanonews;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by louistsai on 26.08.17.
+ */
+public class CategoriesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
